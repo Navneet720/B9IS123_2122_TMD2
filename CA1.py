c6cadc93ddf19005bedee5c5a3b4ad755c54588a
Navneet720 <navneetpandey7208@gmail.com>
2022-04-07 03:08:22 +0100

    Update CA1.py
    calculating  over time hour and pay, Grosspay, regularpay and standardratepay.

--- CA1.py
+++ CA1.py
@@ -1,15 +1,44 @@
-class Employee:
-	 StaiffID=""
-	 FirstName=""
-	 LasttName=""
-	 RegHours=0.0
-	 HourlyRate=0.0
-	 OTMultiple=0
-	 TaxCreadit=0.0
-	 StandardBand=0
-	 s=01;
-	 
+
+class Employee(object):
+
+    def __init__(self, StaffID, FirstName, LastName, RegHours, HourlyRate, OTMultiple, TaxCreadit ,StandardBand):
+     self.StaffID = StaffID
+     self.FirstName = FirstName
+     self.LastName =LastName
+     self.RegHours = RegHours
+     self.HourlyRate =HourlyRate
+     self.OTMultiple =OTMultiple
+     self.TaxCreadit =TaxCreadit
+     self.StandardBand =StandardBand
+
+    def computePayment(self, HourseWokred, date):
+        overTimeHours = 0
+        regularPay = 0
+        overtimepay = 0
+        overTimeHourlyRate = 24
+        grossPay=0
+        standardRatePay=0
+        if HourseWokred > self.RegHours:
+         overTimeHours = HourseWokred - self.RegHours
+         print("overtime Hourse",overTimeHours)
+         print("regular Hourse",self.RegHours)
+
+        if HourseWokred <= self.RegHours:
+            self.RegHours = HourseWokred
+            print("overtime Hourse", overTimeHours)
+            print("regular Hourse", self.RegHours)
+
+        regularPay = self.RegHours * self.HourlyRate
+        overtimepay = overTimeHours * overTimeHourlyRate
+        grossPay = regularPay + overtimepay
+        standardRatePay = grossPay - self.StandardBand
+        
+        print(regularPay)
+        print(overtimepay)
+        print(grossPay)
+        print(standardRatePay)
 
 
-def computePayment():
- return 0 ;
+
+jg= Employee(10501018,'Navneet','Pandey', 37, 16, 1.5, 72, 710)
+jg.computePayment(42,'31/10/2021')
